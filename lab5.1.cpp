bfed874af5233e0eed6b238e0dae42976561758d
risreb <k.salamakha04@mail.ru>
2026-07-17 23:56:53 +0300

    lab5.1
    2 задание

---
+++ lab5.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите первое число: ";
+	int a{};
+	std::cin >> a;
+
+	std::cout << " Введите второе число: ";
+	int b{};
+	std::cin >> b;
+
+	std::cout << " Введите третье число: ";
+	int c{};
+	std::cin >> c;
+
+	int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+
+	int min = (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+
+	int mid = a + b + c - max - min;
+
+	std::cout << "Резльтат: " << max << " " << mid << " " << min;
+
+	return EXIT_SUCCESS;
+}
